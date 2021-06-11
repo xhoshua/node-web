7a7fb602423f64f76442a0b491a220e66c925090
Xhoi <mucaxhoi3@gmail.com>
2021-06-11 13:26:47 +0200

    Add forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,8 @@ const forecast = (latitude , longitude ,callback)=>{
     }else if (body.error){
               callback('Unable to find location',undefined)
     }else {
-          callback(undefined, body.current.weather_descriptions +'. It is currently ' + body.current.temperature +' degress out. There is a '+ body.current.precip + '% chance of rain')}
+     
+          callback(undefined, body.current.weather_descriptions +'. It is currently ' + body.current.temperature +' degress out.  It feels like: '+body.current.feelslike +' degrees out. The humidity is '+ body.current.humidity +'%. There is a '+ body.current.precip + '% chance of rain')}
 })
 }
  module.exports = forecast
